0d12456790c135733d6193bf1f2550c95ba6e918
Rony Yosef <ronyyo@mta.ac.il>
2022-02-15 18:03:02 +0200

    commit for testing git action and the rules working

---
+++ bed.py
@@ -0,0 +1,1 @@
+<<<This file is not a legal Python file>>><<<This file is not a legal Python file>>><<<This file is not a legal Python file>>><<<This file is not a legal Python file>>><<<This file is not a legal Python file>>><<<This file is not a legal Python file>>>
